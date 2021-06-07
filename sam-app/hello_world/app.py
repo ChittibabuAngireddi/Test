c9abbe8d497760413c70119c5b778a1be95f84e9
c.babu.angireddi <c.babu.angireddi@accenture.com>
2021-06-07 13:13:19 +0530

    Revert change

--- sam-app/hello_world/app.py
+++ sam-app/hello_world/app.py
@@ -37,7 +37,7 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "hello world 123",
+                "message": "hello world",
                 # "location": ip.text.replace("\n", "")
             }
         ),
